39006416ab8112174c0d2d3a0ba6f88da43a0f40
Richard Wooding <richard.wooding@spandigital.com>
2023-04-21 16:03:13 +0200

    Made public

--- cmd/list.go
+++ cmd/list.go
@@ -11,14 +11,9 @@ import (
 
 // listPromptsCmd represents the listPrompts command
 var listPromptsCmd = &cobra.Command{
-	Use:   "list-prompts",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "list",
+	Short: "List prompts from prompt database",
+	Long:  `List prompts from prompt database.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		for _, library := range model.Libraries {
 			for _, command := range library.Commands {
--- cmd/run.go
+++ cmd/run.go
@@ -14,14 +14,9 @@ import (
 
 // runPromptsCmd represents the runPrompts command
 var runPromptsCmd = &cobra.Command{
-	Use:   "run-prompts",
+	Use:   "run",
 	Short: "Run prompts from prompt database",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Run prompts from prompt database.`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		commandInstance, err := model.NewCommandInstance(args)
 		if err != nil {
