19f8d5a216146f82a081a6919669277bd6a5e0de
Richard Wooding <richard.wooding@spandigital.com>
2023-05-10 17:57:12 +0200

    Made better

---
+++ web/build-ui.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+yarn parcel build src/index.html --public-url ./
--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
+import Dashboard from "./dashboard";
 
 const container = document.getElementById("app");
 const root = createRoot(container)
-root.render(<App />);
+root.render(<Dashboard />);
