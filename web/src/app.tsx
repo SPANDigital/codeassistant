788bd1ebd5e0515cfa90e8c035c3e4882ec6cda4
Richard Wooding <richard.wooding@spandigital.com>
2023-05-16 08:38:08 +0200

    SPAN Digital

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -36,8 +36,8 @@ function Copyright(props: any) {
     return (
         <Typography variant="body2" color="text.secondary" align="center" {...props}>
     {'Copyright © '}
-    <Link color="inherit" href="https://mui.com/">
-        Your Website
+    <Link color="inherit" href="https://spandigital.com/">
+        SPAN Digital
     </Link>{' '}
     {new Date().getFullYear()}
     {'.'}
