93b56fbae5f74c43ba8f9e3101e99e559266f43d
Richard Wooding <richard.wooding@spandigital.com>
2023-07-06 11:17:52 +0200

    Changed with omitempty (#47)

--- client/openai/completionsrequest.go
+++ client/openai/completionsrequest.go
@@ -10,7 +10,7 @@ type completionsRequest struct {
 	Model       string         `json:"model"`
 	Messages    []model.Prompt `json:"messages"`
 	User        *string        `json:"user"`
-	Temperature *float32       `json:"temperature"`
-	TopP        *float32       `json:"top_p"`
+	Temperature *float32       `json:"temperature,omitempty"`
+	TopP        *float32       `json:"top_p,omitempty"`
 	Stream      bool           `json:"stream"`
 }
